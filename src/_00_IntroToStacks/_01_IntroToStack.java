68aab7e032f4ea9d5a729417df3eb1485e9990d3
EchoHunter <48270082+EchoHunter@users.noreply.github.com>
2022-02-10 18:31:03 -0800

    :b:

--- src/_00_IntroToStacks/_01_IntroToStack.java
+++ src/_00_IntroToStacks/_01_IntroToStack.java
@@ -9,14 +9,26 @@ public class _01_IntroToStack {
     public static void main(String[] args) {
         // 1. Create a Stack of Doubles
         //    Don't forget to import the Stack class
-
+Stack<Double> d = new Stack<Double>();
         // 2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
-
+Random r = new Random();
+for (int i = 0; i <= 100; i++) {
+	d.push((double) r.nextInt(100));
+}
         // 3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
-
+String input = JOptionPane.showInputDialog("Pick 2 numbers between 0 and 100, inclusive");
         // 4. Pop all the elements off of the Stack. Every time a double is popped that is
         //    between the two numbers entered by the user, print it to the screen.
-
+String regEx = "[a-zA-Z,\\s]";
+StringBuilder finalInput = new StringBuilder(input.replaceAll(regEx, ""));
+finalInput.insert(0, '[');
+finalInput.insert(2, '-');
+finalInput.append(']');
+Double s;
+for (int i = 0; i < d.size(); i++) {
+	s = d.pop();
+	
+}
 
         // EXAMPLE:
         // NUM 1: 65
