8acb0b77c727063ed853797f8cae5a10652da764
League Student <league@iMac-24.attlocal.net>
2022-04-07 18:29:46 -0700

    :b:
    Worked on hangman, did not get it yet, currently working on getting win & loss to work

--- src/_03_Hangman/Hangman.java
+++ src/_03_Hangman/Hangman.java
@@ -1,5 +1,8 @@
 package _03_Hangman;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
 import java.util.Stack;
 
 import javax.swing.JFrame;
@@ -7,23 +10,75 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class Hangman {
+public class Hangman implements KeyListener {
 JPanel panel;
 JFrame frame;
 JLabel label;
-	public void run() {
+Stack <String> hangWord = new Stack();
+String hWHolder = "";
+StringBuilder blankHWHolder = new StringBuilder();	
+int guesses = 10;
+ArrayList <Character> keyAlreadyPressed = new ArrayList<Character>();
+public void run() {
 		panel = new JPanel();
 		frame = new JFrame();
 		label = new JLabel();
 		panel.add(label);
 		frame.add(panel);
-	Utilities u = new Utilities ();
-	Stack <String> hangWord = new Stack();
-	String numLengthS = JOptionPane.showInputDialog("Give me a number that is 266 or less, no negatives or 0");
-	int numLength = Integer.parseInt(numLengthS);
-	for (int i = 0; i < numLength+1; i++) {
-		hangWord.add(u.readRandomLineFromFile("dictionary.txt"));
-	}
+		frame.pack();
+		frame.addKeyListener(this);
+		frame.setVisible(true);
+	String placeHolder = "";
 	
-}
+	String numLengthS = JOptionPane.showInputDialog("Give me a number that is 1-266");
+	int numLength = Integer.parseInt(numLengthS);
+	while (hangWord.size() < numLength) {
+		placeHolder = Utilities.readRandomLineFromFile("dictionary.txt");
+		if (!hangWord.contains(placeHolder)) {
+			hangWord.add(placeHolder);
+		}
+	}
+	while ((guesses > 0)||(hangWord.size() >0)) {
+		if (!blankHWHolder.toString().contains("_")) {
+			startNewGame();
+		}
+	}
+	if (guesses <= 0) {
+		JOptionPane.showMessageDialog(null, "you lost");
+	}
+	if(hangWord.size()<=0) {
+	JOptionPane.showMessageDialog(null, "You won, very cool");
+	}
+	}
+	void startNewGame() {
+	hWHolder = hangWord.pop();
+	for (int j = 0; j < hWHolder.length(); j++) {
+		blankHWHolder.append("_");
+	}
+	label.setText(blankHWHolder.toString() + "  Guesses left: " + guesses);
+	
+	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		guesses--;
+		for (int i = 0; i < hWHolder.length(); i++) {
+			if ((hWHolder.charAt(i) == e.getKeyChar())&&(!keyAlreadyPressed.contains(e.getKeyChar()))) {
+				blankHWHolder.replace(i, i+1, String.valueOf(e.getKeyChar()));
+				guesses++;
+			}
+			label.setText(blankHWHolder.toString()+ "  Guesses left: " + guesses);
+		}
+		keyAlreadyPressed.add(e.getKeyChar());
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
---
+++ src/_03_Hangman/HangmanRunner.java
@@ -0,0 +1,8 @@
+package _03_Hangman;
+
+public class HangmanRunner {
+public static void main(String[] args) {
+	Hangman h = new Hangman();
+	h.run();
+}
+}
