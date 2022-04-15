eebf4f2f71c7202460a7f767d7744c8fd29394a3
League Student <league@iMac-11.attlocal.net>
2022-04-14 18:29:47 -0700

    :b:
    hangman

--- src/_03_Hangman/Hangman.java
+++ src/_03_Hangman/Hangman.java
@@ -18,8 +18,15 @@ Stack <String> hangWord = new Stack();
 String hWHolder = "";
 StringBuilder blankHWHolder = new StringBuilder();	
 int guesses = 10;
+boolean repeat = true;
 ArrayList <Character> keyAlreadyPressed = new ArrayList<Character>();
 public void run() {
+		while (repeat) {
+			repeat = false;
+			guesses = 10;
+			hWHolder = "";
+			hangWord.clear();
+		
 		panel = new JPanel();
 		frame = new JFrame();
 		label = new JLabel();
@@ -42,22 +49,43 @@ public void run() {
 		if (!blankHWHolder.toString().contains("_")) {
 			startNewGame();
 		}
-	}
+	
 	if (guesses <= 0) {
 		JOptionPane.showMessageDialog(null, "you lost");
+		int yn = JOptionPane.showConfirmDialog(null, "Would you like to try again?");
+		if (yn == 1) {
+			System.exit(0);
+		}
+		else if(yn == 0) {
+			repeat = true;
+			frame.setVisible(false);
+		}
+		
 	}
-	if(hangWord.size()<=0) {
+	if((hangWord.size()<0)&&(guesses > 0)) {
 	JOptionPane.showMessageDialog(null, "You won, very cool");
+	int yn = JOptionPane.showConfirmDialog(null, "Would you like to try again?");
+	if (yn == 1) {
+		System.exit(0);
+	}
+	else if(yn == 0) {
+		repeat = true;
+		frame.setVisible(false);
 	}
 	}
+	}
+	}
+}
 	void startNewGame() {
 	hWHolder = hangWord.pop();
+	keyAlreadyPressed.clear();
 	for (int j = 0; j < hWHolder.length(); j++) {
 		blankHWHolder.append("_");
 	}
 	label.setText(blankHWHolder.toString() + "  Guesses left: " + guesses);
 	
 	}
+
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
