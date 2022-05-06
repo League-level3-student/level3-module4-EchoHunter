9b02b2ff73f1f8379956b7b20ab6dbd7f6f3d36f
League Student <league@iMac-34.attlocal.net>
2022-05-05 18:28:27 -0700

    :b:
    megalomanialovania

--- src/_04_Maze_Maker/Maze.java
+++ src/_04_Maze_Maker/Maze.java
@@ -1,31 +1,41 @@
 package _04_Maze_Maker;
 import java.awt.Graphics;
+import java.util.ArrayList;
 
 public class Maze {
     // 1. Create a 2D array of cells. Don't initialize it.
     
     private int rows;
     private int cols;
-
+    Cell[][] cellGroups;
     public Maze(int rows, int cols) {
         this.rows = rows;
         this.cols = cols;
 
         // 2. Initialize the cells array using the rows and cols variables
-
+        cellGroups = new Cell[rows][cols];
         // 3. Iterate through each cell and initialize it
         //    using row and col as the cell location
-
+        for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				cellGroups[i][j] = new Cell(i,j);
+			}
+		}
     }
 
     // 4. This method iterates through the cells and draws them
     public void draw(Graphics g) {
-
+    	for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				cellGroups[i][j].draw(g);
+			}
+		}
     }
 
     // 5. This method returns the selected cell
     public Cell getCell(int row, int col){
-        return  null;
+        
+    	return  cellGroups[row][col];
     }
 
     public int getRows() {
