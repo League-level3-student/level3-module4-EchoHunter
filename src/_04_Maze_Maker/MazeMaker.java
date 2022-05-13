08c73b5307e9db54a3385cc67fde32fc92aa9759
WriterArtistCoder <tinystripz@outlook.com>
2022-05-12 18:02:19 -0700

    :b:
    Finally finished mazeMaker, apparently missed some methods that were already in the code

--- src/_04_Maze_Maker/MazeMaker.java
+++ src/_04_Maze_Maker/MazeMaker.java
@@ -23,10 +23,10 @@ public class MazeMaker {
         //    This will be the starting point. Then select a random cell along
         //    the opposite wall and remove its exterior wall. This will be the
         //    finish line.
-        maze.cellGroups[0][randGen.nextInt(cols)].setWestWall(false);
-        maze.cellGroups[rows][randGen.nextInt(cols)].setEastWall(false);
+        maze.cellGroups[0][randGen.nextInt(cols)].setNorthWall(false);
+        maze.cellGroups[rows-1][randGen.nextInt(cols)].setSouthWall(false);
         // 2. select a random cell in the maze to start 
-        selectNextPath(maze.cellGroups[randGen.nextInt(rows)][randGen.nextInt(cols)]);
+        selectNextPath(maze.cellGroups[randGen.nextInt(rows-1)+1][randGen.nextInt(cols-1)+1]);
         // 3. call the selectNextPath method with the randomly selected cell
 
         return maze;
@@ -37,42 +37,13 @@ public class MazeMaker {
         // A. SET currentCell as visited
 currentCell.setBeenVisited(true);
         // B. check for unvisited neighbors using the cell
-Boolean[] neighborCheck = new Boolean[8];
-ArrayList<Integer> trues = new ArrayList<Integer>();
-for (int i = 0; i < neighborCheck.length; i++) {
-	if ((!maze.cellGroups[currentCell.getRow()+1][currentCell.getCol()+1].hasBeenVisited())||
-		(!maze.cellGroups[currentCell.getRow()-1][currentCell.getCol()+1].hasBeenVisited())||
-		(!maze.cellGroups[currentCell.getRow()+1][currentCell.getCol()-1].hasBeenVisited())||
-		(!maze.cellGroups[currentCell.getRow()-1][currentCell.getCol()-1].hasBeenVisited())||
-		(!maze.cellGroups[currentCell.getRow()][currentCell.getCol()+1].hasBeenVisited())||
-		(!maze.cellGroups[currentCell.getRow()+1][currentCell.getCol()].hasBeenVisited())||
-		(!maze.cellGroups[currentCell.getRow()-1][currentCell.getCol()].hasBeenVisited())||
-		(!maze.cellGroups[currentCell.getRow()][currentCell.getCol()-1].hasBeenVisited())) {
-		neighborCheck[i] = true;
-		trues.add(i);
-	}
-	else {
-		neighborCheck[i] = false;
-	}
-}
+ArrayList<Cell> cs = getUnvisitedNeighbors(currentCell);
         // C. if has unvisited neighbors,
-int numStor = trues.get(randGen.nextInt(trues.size()));
-if (numStor == 0) {
-	uncheckedCells.push(maze.cellGroups[currentCell.getRow()+1][currentCell.getCol()+1]);
-}else if(numStor == 1) {
-	uncheckedCells.push(maze.cellGroups[currentCell.getRow()-1][currentCell.getCol()+1]);
-}else if(numStor == 2) {
-	uncheckedCells.push(maze.cellGroups[currentCell.getRow()+1][currentCell.getCol()-1]);
-}else if(numStor == 3) {
-	uncheckedCells.push(maze.cellGroups[currentCell.getRow()-1][currentCell.getCol()-1]);
-}else if(numStor == 4) {
-	uncheckedCells.push(maze.cellGroups[currentCell.getRow()][currentCell.getCol()+1]);
-}else if(numStor == 5) {
-	uncheckedCells.push(maze.cellGroups[currentCell.getRow()+1][currentCell.getCol()]);
-}else if(numStor == 6) {
-	uncheckedCells.push(maze.cellGroups[currentCell.getRow()][currentCell.getCol()-1]);
-}else if(numStor == 7) {
-	uncheckedCells.push(maze.cellGroups[currentCell.getRow()-1][currentCell.getCol()]);
+if (cs.size()>0) {
+	uncheckedCells.push(cs.get(randGen.nextInt(cs.size())));
+	removeWalls(currentCell,uncheckedCells.peek());
+	currentCell = uncheckedCells.peek();
+	selectNextPath(currentCell);
 }
         // C1. select one at random.
 
@@ -86,7 +57,14 @@ if (numStor == 0) {
 
 
         // D. if all neighbors are visited
-
+else {
+	if (uncheckedCells.size() > 0) {
+		
+	
+	currentCell = uncheckedCells.pop();
+	selectNextPath(currentCell);
+	}
+	}
         // D1. if the stack is not empty
 
         // D1a. pop a cell from the stack
